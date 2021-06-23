e33403d93d24109fb17ab1350994a977145e10f6
tomasz.su <tomasz.su@gmail.com>
2021-06-23 11:32:55 +0200

    [Fix] change zone for vpc too

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,7 +24,7 @@ module "vpc" {
     {
       subnet_name   = "${var.env}-subnet-01"
       subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.0.0/16"
-      subnet_region = "us-east1"
+      subnet_region = "us-east4"
     },
   ]
 
