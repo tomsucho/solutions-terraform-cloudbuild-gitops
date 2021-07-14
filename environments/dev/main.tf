53981e865b32e188eb11a286f8ed2f4d2193b09a
tomsucho <tomasz.su@gmail.com>
2021-07-14 11:56:03 +0000

    [REM] test VM to dev env

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -35,18 +35,18 @@ provider "google" {
 }
 
 module "vpc" {
-  source      = "../../modules/vpc"
-  project     = var.project
-  env         = local.env
+  source  = "../../modules/vpc"
+  project = var.project
+  env     = local.env
 
   subnet_cidr = "10.10.0.0/16"
 }
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = var.project
-  subnet  = module.vpc.subnet
-}
+# module "http_server" {
+#   source  = "../../modules/http_server"
+#   project = var.project
+#   subnet  = module.vpc.subnet
+# }
 
 module "firewall" {
   source  = "../../modules/firewall"
